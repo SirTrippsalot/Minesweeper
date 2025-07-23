5586f4304332690ad27d8731622abfce42594a2c
SirTrippsalot <myoumans@gmail.com>
2025-07-22 21:30:49 -0600

    Add graph board classes

---
+++ app/src/main/java/com/edgefield/minesweeper/graph/Cell.kt
@@ -0,0 +1,14 @@
+package com.edgefield.minesweeper.graph
+
+/**
+ * Basic representation of a board cell.
+ */
+data class Cell(
+    val id: String,
+    val vertices: Set<String>,
+    var isMine: Boolean = false,
+    var isRevealed: Boolean = false,
+    var isFlagged: Boolean = false,
+    val neighbors: MutableSet<String> = mutableSetOf()
+)
+
